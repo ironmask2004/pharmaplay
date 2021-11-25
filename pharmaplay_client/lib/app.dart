8cb92bc9743fdce052ae1f529670ed0265f7341e
ironmask2004 <ironmask2004@gmail.com>
2021-11-25 11:05:37 +0200

    jkfhsjkda

---
+++ pharmaplay_client/lib/app.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pharmaplay_client/src/authentication/authentication.dart';
+import 'package:pharmaplay_client/src/home/home.dart';
+import 'package:pharmaplay_client/src/login/login.dart';
+import 'package:';
+//authentication_repository/lib/authentication_repository.dart';
+import 'package:pharmaplay_client/packages/user_repository/lib/user_repository.dart';
+import 'package:pharmaplay_client/src/splash/splash.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+    required this.authenticationRepository,
+    required this.userRepository,
+  }) : super(key: key);
+
+  final AuthenticationRepository authenticationRepository;
+  final UserRepository userRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+          userRepository: userRepository,
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            print(child.toString());
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                print(child.toString());
+                _navigator.pushAndRemoveUntil<void>(
+                  HomePage.route(),
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unauthenticated:
+                print(child.toString());
+                _navigator.pushAndRemoveUntil<void>(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+                break;
+              default:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
+  }
+}
---
+++ pharmaplay_client/lib/src/login/login.dart
@@ -0,0 +1,8 @@
+export 'bloc/login_bloc.dart';
+export 'models/models.dart';
+export 'view/view.dart';
+
+class test1 {
+  final int t1 = 0;
+  final int t2 = 0;
+}
