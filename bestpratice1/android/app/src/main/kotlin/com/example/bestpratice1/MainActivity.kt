c358ab18be9346e206a019035687f462ae3ae77e
ironmask2004 <ironmask2004@gmail.com>
2022-01-26 02:13:30 +0200

    add best practis Project

---
+++ bestpratice1/android/app/src/main/kotlin/com/example/bestpratice1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bestpratice1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bestpratice1/lib/src/settings/settings_controller.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'settings_service.dart';
+
+/// A class that many Widgets can interact with to read user settings, update
+/// user settings, or listen to user settings changes.
+///
+/// Controllers glue Data Services to Flutter Widgets. The SettingsController
+/// uses the SettingsService to store and retrieve user settings.
+class SettingsController with ChangeNotifier {
+  SettingsController(this._settingsService);
+
+  // Make SettingsService a private variable so it is not used directly.
+  final SettingsService _settingsService;
+
+  // Make ThemeMode a private variable so it is not updated directly without
+  // also persisting the changes with the SettingsService.
+  late ThemeMode _themeMode;
+  late Locale _UIlocale;
+
+  // Allow Widgets to read the user's preferred ThemeMode.
+  ThemeMode get themeMode => _themeMode;
+  Locale get UIlocale => _UIlocale;
+
+  /// Load the user's settings from the SettingsService. It may load from a
+  /// local database or the internet. The controller only knows it can load the
+  /// settings from the service.
+  Future<void> loadSettings() async {
+    _themeMode = await _settingsService.themeMode();
+    _UIlocale = await _settingsService.uiLocale();
+
+    // Important! Inform listeners a change has occurred.
+    notifyListeners();
+  }
+
+  /// Update and persist the ThemeMode based on the user's selection.
+  Future<void> updateThemeMode(ThemeMode? newThemeMode) async {
+    if (newThemeMode == null) return;
+
+    // Dot not perform any work if new and old ThemeMode are identical
+    if (newThemeMode == _themeMode) return;
+
+    // Otherwise, store the new theme mode in memory
+    _themeMode = newThemeMode;
+
+    // Important! Inform listeners a change has occurred.
+    notifyListeners();
+
+    // Persist the changes to a local database or the internet using the
+    // SettingService.
+    await _settingsService.updateThemeMode(newThemeMode);
+  }
+
+  Future<void> updateUILocale(Locale? newUILocale) async {
+    if (newUILocale == null) return;
+
+    // Dot not perform any work if new and old ThemeMode are identical
+    if (newUILocale == _UIlocale) return;
+
+    // Otherwise, store the new theme mode in memory
+    _UIlocale = newUILocale;
+
+    // Important! Inform listeners a change has occurred.
+    notifyListeners();
+
+    // Persist the changes to a local database or the internet using the
+    // SettingService.
+    await _settingsService.updateUILocale(newUILocale);
+  }
+}
+
+
+/*
+abstract class UILocale {
+  static String currentLocale = '';
+
+  static changeUILocale() async {
+    currentLocale =
+        await MySharedPreferences.instance.getStringValue("UILocale");
+    currentLocale = currentLocale == 'ar' ? 'en' : 'ar';
+
+    await MySharedPreferences.instance
+        .setStringValue("UILocale", currentLocale);
+    print(' LAnguge UI Changed to : $currentLocale ');
+  }
+
+  static Future<String> getUILocale() async {
+    currentLocale =
+        await MySharedPreferences.instance.getStringValue("UILocale");
+    currentLocale = currentLocale == 'ar' ? 'en' : 'ar';
+
+    print(' LAnguge UI     : $currentLocale ');
+    return (currentLocale);
+  }
+}
+
+*/
