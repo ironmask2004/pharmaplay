5d5e534cea7bf2f146c372ef977c3be3093f1882
ironmask2004 <ironmask2004@gmail.com>
2022-01-31 23:44:22 +0200

    setting bloc Working Fine

--- pharmaplay_client/lib/src/settings/bloc/settings_state.dart
+++ pharmaplay_client/lib/src/settings/bloc/settings_state.dart
@@ -1,7 +1,7 @@
 part of 'settings_bloc.dart';
 
 @immutable
-abstract class SettingsState extends Equatable {
+class SettingsState extends Equatable {
   const SettingsState(
       {this.uiLocale = const Locale('en'),
       this.uiThemeMode = ThemeMode.system});
@@ -9,14 +9,14 @@ abstract class SettingsState extends Equatable {
   final Locale uiLocale;
   final ThemeMode uiThemeMode;
 
-  /*SettingsState copyWith({
+  SettingsState copyWith({
     Locale? uiLocale,
     ThemeMode? uiThemeMode,
   }) {
     return SettingsState(
         uiLocale: uiLocale ?? this.uiLocale,
         uiThemeMode: uiThemeMode ?? this.uiThemeMode);
-  }*/
+  }
 
   @override
   List<Object> get props => [
@@ -27,7 +27,7 @@ abstract class SettingsState extends Equatable {
 
 class SettingsInitial extends SettingsState {}
 
-class SettingsStateUILocaleChanged extends SettingsState {
+/*class SettingsStateUILocaleChanged extends SettingsState {
   const SettingsStateUILocaleChanged(Locale uiLocale)
       : super(uiLocale: uiLocale);
 }
@@ -36,3 +36,4 @@ class SettingsStateUIThemeModeChanged extends SettingsState {
   const SettingsStateUIThemeModeChanged(ThemeMode uiThemeMode)
       : super(uiThemeMode: uiThemeMode);
 }
+*/
